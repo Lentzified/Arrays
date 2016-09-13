db64fd96cc8c07e7cffc68ef576c9859af864eec
Adam Richard Lentz <lentzar@dcsdk12.org>
2016-09-13 13:50:06 -0600

    Created and tested the mod_three? method for arrays.rb

--- arrays.rb
+++ arrays.rb
@@ -14,5 +14,26 @@ def have_three?(a)
 	false
 end
 
-puts have_three?([1,2,3,3,3,4,5,6]) #true
-puts have_three?([1,2,3,4,5,6,7,8]) #false
+def mod_three?(a)
+
+	odd_ints = 0
+	even_ints = 0
+	a.each do |n|
+		if (n % 2) == 0
+			even_ints += 1
+			odd_ints = 0
+			if even_ints == 3
+				return true
+			end
+		else
+			even_ints == 0
+			odd_ints += 1
+			if odd_ints == 3
+				return true
+			end
+		end
+
+	end
+
+	false
+end
